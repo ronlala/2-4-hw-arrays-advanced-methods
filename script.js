d4b4ab9777213624f616dd5ca23a12655b4860db
La-Ronda <lalacha@macbookpro.mynetworksettings.com>
2025-02-23 17:05:07 -0600

    Finished up to number 15

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ let startsLettera = groceryList.filter((item) => item.startsWith("A"));
   // 9
   // Check if some of the items in the schoolList array ends with letter "s"
   
-  let letterEends = schoolList.filter((listItem) => listIem.incldues("s"));
+  let endswithe = schoolList.filter((lastschool) => lastschool.includes("s"));
 
   // Use the following array to complete the tasks using the appropriate array method.
   let patronAges = [17, 21, 5, 85, 55, 15, 19, 9, 33, 12, 54, 42, 60, 13, 6];
@@ -101,29 +101,51 @@ let startsLettera = groceryList.filter((item) => item.startsWith("A"));
   // 10
   // Check if every age in the patronAges array a positive
   
+  
   // 11
-  // Create a NEW array called "kidsDiscountAges" that contains the elements from the "patronAges" array that are 12 or under. The "patronAges" array should stay the same. Print "patronAges" and "kidsDiscountAges" to the console to verify your results.
+  // Create a NEW array called "kidsDiscountAges" that contains the elements from the "patronAges" array that are 12 or under. T
+  // he "patronAges" array should stay the same. Print "patronAges" and "kidsDiscountAges" to the console to verify your results.
+
+  let kidsDiscountAges = patronAges.filter((twelveUnder) => twelveUnder < 12);
+  console.log(patronAges);
+  console.log(kidsDiscountAges);
   
   // 12
   // Find the first element from the "patronAges" array that are 12 or under.
+
+  let firstElementUndertwelve = patronAges.find((twelveUnderfirst) => twelveUnderfirst < 12);
+
+
   
   // 13
-  // Create a NEW array called "seniorsDiscountAges" that contains the elements from the "patronAges" array that are 60 or over. The "patronAges" array should stay the same. Print "patronAges" and "seniorsDiscountAges" to the console to verify your results.
+  // Create a NEW array called "seniorsDiscountAges" that contains the elements from the "patronAges" array that are 60 or over. 
+  // The "patronAges" array should stay the same. Print "patronAges" and "seniorsDiscountAges" to the console to verify your results.
+
+  let seniorsDiscountAges = patronAges.filter((ageoversixty) => ageoversixty >= 60);
   
   // 14
   // Find the first element from the "patronAges" array that are 60 or over.
-  
+  let firstElementoversixty = patronAges.find((ageoversixty2) => ageoversixty2 >= 60);
+  console.log(firstElementUndertwelve);
+
   // 15
-  // Create a NEW array called "patronAgesIn2" that contains all the age elements from the patronAges array, but with 2 years added to each. The "patronAges" array should stay the same. Print patronAges and patronAgesIn2 to the console to verify your results.
-  
+  // Create a NEW array called "patronAgesIn2" that contains all the age elements from the patronAges array, but with 2 years added to each.
+  //  The "patronAges" array should stay the same. Print patronAges and patronAgesIn2 to the console to verify your results.
+ let patronAgesIn2 = patronAges.map((addtwoyears) => addtwoyears +2);
+ console.log(patronAgesIn2);
+ console.log(patronAges);
+
+    // patronAges.forEach(add2yearstoages);
   // 16
-  // Call a method on the patronAges array that will print: `patron age: ${age}` for each element in the array to the console. For example, the first item will be "patron age: 17" because 17 is the first element in patronAges. The "patronAges" array should not be affected. Print patronAges to be sure.
+  // Call a method on the patronAges array that will print: `patron age: ${age}` for each element in the array to the console. 
+  // For example, the first item will be "patron age: 17" because 17 is the first element in patronAges. The "patronAges" array should not be affected. Print patronAges to be sure.
   
   // 17
   // Find the average age in the patronsAge array. Do this by adding all the ages together and then dividing by the number of elements in the array. (Hint: Do not use loops);
   
   // 18
   // Sort the patronAges from lowest to highest
+    
   
   // 19
   // Sort the patronAges from highest to lowest
